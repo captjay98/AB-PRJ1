524a66d89ea3745e9a252ae7ca3827a3c6725eb3
captjay98wsl <captjay98@gmail.com>
2023-04-06 22:50:59 +0100

    Added Employer and Job Model

--- api/employers/admin.py
+++ api/employers/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import EmployerProfile, Job
 
 # Register your models here.
+admin.site.register(EmployerProfile)
+admin.site.register(Job)
--- api/employers/models.py
+++ api/employers/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.fields import related
+from django.utils import timezone
 
 # Create your models here.
+
+
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="EmployerProfile",
+        related_name="EmployerProfile",
+    )
+
+
+class Job(models.Model):
+    job_recruiter = models.ForeignKey(
+        EmployerProfile,
+        on_delete=models.DO_NOTHING,
+        related_name="jobRecruiter",
+    )
+    title = models.CharField(
+        verbose_name="Job title",
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+
+    description = models.TextField(
+        verbose_name="Job description",
+        max_length=555,
+        blank=True,
+        null=True,
+    )
+
+    required_skills = models.TextField(
+        verbose_name="Required Skills",
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+
+    location = models.CharField(
+        verbose_name="Location",
+        max_length=55,
+        blank=True,
+        null=True,
+    )
+
+    industry = models.CharField(
+        verbose_name="Industry",
+        max_length=155,
+        blank=True,
+        null=True,
+    )
+
+    date_posted = models.DateTimeField(
+        default=timezone.now,
+    )
+
+    def __str__(self) -> str:
+        return self.title
