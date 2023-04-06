65c55943ca0cccf9905765ed4a77c000ed138af2
captjay98wsl <captjay98@gmail.com>
2023-04-06 18:35:22 +0100

    created A SeekerProfile linked to a user on registration a skill model, a quali and expe models

--- api/seekers/admin.py
+++ api/seekers/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from .models import SeekerProfile, Skill, Qualification, Experience
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# Register your models her.
+
+
+class SeekerProfileAdmin(UserAdmin):
+    list_display = (
+        "user",
+        "phone_number",
+        "city",
+        "state",
+        "country",
+        "ethnicity",
+        # "skills",
+        "cv",
+        "passport",
+        "visa",
+    )
+    search_fields = (
+        "user",
+        "phone_number",
+        "city",
+        "state",
+        "country",
+        "ethnicity",
+    )
+
+    filter_horizontal = ()
+
+    list_filter = (
+        "state",
+        "country",
+        "ethnicity",
+    )
+
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = {
+        "name",
+    }
+
+
+class QualificationAdmin(admin.ModelAdmin):
+    list_display = {
+        "title",
+        "body",
+    }
+
+
+class RegisterAdmin(admin.ModelAdmin):
+    list_display = {
+        "title",
+        "body",
+    }
+
+
+admin.site.register(SeekerProfile)
+admin.site.register(Skill)
+admin.site.register(Qualification)
+admin.site.register(Experience)
--- api/seekers/models.py
+++ api/seekers/models.py
@@ -1,3 +1,172 @@
 from django.db import models
+from django.db.models.fields import related
+from users.models import User
+from django.conf import settings
+from django_countries.fields import CountryField
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+ETHCHOICES = (
+    ("Ethnicity", "Ethnicity"),
+    ("African", "African"),
+    ("African American", "African American"),
+    ("Arab", "Arab"),
+    ("Asian", "Asian"),
+    ("Any other Asian background", "Any other Asian background"),
+    ("Bangladeshi", "Bangladeshi"),
+    ("Caribbean", "Caribbean"),
+    ("Chinese", "Chinese"),
+    ("English", "English"),
+    ("Gypsy or Irish Traveller", "Gypsy or Irish Traveller"),
+    ("Hispanic", "Hispanic"),
+    ("Indian", "Indian"),
+    ("Irish", "Irish"),
+    ("Mixed or Multiple background", "Mixed or Multiple background"),
+    ("Mixed or Multiple ethnic groups", "Mixed or Multiple ethnic groups"),
+    ("Native American", "Native American"),
+    ("Northern Irish", "Northern Irish"),
+    ("Pacific Islander", "Pacific Islander"),
+    ("Pakistani", "Pakistani"),
+    ("Scottish", "Scotish"),
+    ("White", "White"),
+    ("White and Black African", "White and Black African"),
+    ("White and Black Caribbean", "White and Black Caribbean"),
+    ("Welsh", "Welsh"),
+)
+
+
+def passport_filepath(self, filename):
+    return f"passports/{self.id}/passport.pdf"
+
+
+def cv_filepath(self, filename):
+    return f"cvs/{self.id}/cv.pdf"
+
+
+def visa_filepath(self, filename):
+    return f"visas/{self.id}/visa.pdf"
+
+
+class Skill(models.Model):
+    name = models.CharField(
+        verbose_name=_("skill"),
+        max_length=55,
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class SeekerProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="SeekerProfile",
+    )
+
+    phone_number = models.CharField(
+        verbose_name=_("Phone number"),
+        max_length=50,
+        blank=True,
+        null=True,
+        unique=True,
+    )
+
+    city = models.CharField(
+        verbose_name=_("City"),
+        max_length=50,
+        blank=True,
+        null=True,
+    )
+
+    state = models.CharField(
+        verbose_name=_("State "),
+        max_length=50,
+        blank=True,
+        null=True,
+    )
+
+    country = CountryField(
+        verbose_name=_("Country"),
+        max_length=50,
+        blank=True,
+        null=True,
+    )
+
+    ethnicity = models.CharField(
+        verbose_name=_("Ethnicity"),
+        max_length=50,
+        choices=ETHCHOICES,
+        blank=True,
+        null=True,
+    )
+
+    skills = models.ManyToManyField(
+        Skill,
+        verbose_name=_("Skills"),
+        related_name="seeker_profiles",
+        blank=True,
+    )
+
+    cv = models.FileField(
+        upload_to=cv_filepath,
+        blank=True,
+        null=True,
+    )
+
+    passport = models.FileField(
+        upload_to=passport_filepath,
+        blank=True,
+        null=True,
+    )
+
+    visa = models.FileField(
+        upload_to=visa_filepath,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+
+class Qualification(models.Model):
+    seeker_profile = models.ForeignKey(
+        SeekerProfile,
+        on_delete=models.CASCADE,
+        related_name="qualifications",
+        default=None,
+    )
+    title = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+    body = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Experience(models.Model):
+    seeker_profile = models.ForeignKey(
+        SeekerProfile,
+        on_delete=models.CASCADE,
+        related_name="experiences",
+        default=None,
+    )
+    title = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+    body = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return f"{self.title}"
