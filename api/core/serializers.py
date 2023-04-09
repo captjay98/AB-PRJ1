8eca1293c2c599c813ac716785c840c4652d6525
captjay98wsl <captjay98@gmail.com>
2023-04-09 18:01:16 +0100

    Implemented Search Feature to allow users to Sarch for jobs based on Industry and Location

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Faq, Article, InterviewHelp
+from employers.models import Job
 
 
 class FaqSerializer(serializers.ModelSerializer):
@@ -18,3 +19,20 @@ class InterviewHelpSerializer(serializers.ModelSerializer):
     class Meta:
         model = InterviewHelp
         fields = ["__all__"]
+
+
+class JobFilterSerializer(serializers.ModelSerializer):
+    industries = serializers.ListField(
+        child=serializers.CharField(),
+        required=False,
+        help_text="Filter by industries.",
+    )
+    locations = serializers.ListField(
+        child=serializers.CharField(),
+        required=False,
+        help_text="Filter by locations.",
+    )
+
+    class Meta:
+        model = Job
+        fields = ["industries", "locations"]
--- api/core/views.py
+++ api/core/views.py
@@ -3,8 +3,12 @@ from .serializers import (
     FaqSerializer,
     ArticleSerializer,
     InterviewHelpSerializer,
+    JobFilterSerializer,
 )
+from employers.models import Job
+from employers.serializers import JobSerializer
 from rest_framework import generics
+from rest_framework.views import APIView
 from .models import Faq, Article, InterviewHelp
 
 
@@ -18,9 +22,29 @@ class FaqView(generics.ListAPIView):
 
 class ArticleView(generics.ListAPIView):
     queryset = Article.objects.all()
-    serializer_class = FaqSerializer
+    serializer_class = ArticleSerializer
 
 
 class InterviewHelpView(generics.ListAPIView):
     queryset = InterviewHelp.objects.all()
-    serializer_class = FaqSerializer
+    serializer_class = InterviewHelpSerializer
+
+
+class JobSearchView(generics.ListAPIView):
+    serializer_class = JobSerializer
+
+    def get_queryset(self):
+        queryset = Job.objects.all()
+        serializer = JobFilterSerializer(data=self.request.query_params)
+
+        if serializer.is_valid():
+            industry = serializer.validated_data.get("industry")
+            location = serializer.validated_data.get("location")
+
+            if industry:
+                queryset = queryset.filter(industry=industry)
+
+            if location:
+                queryset = queryset.filter(location=location)
+
+        return queryset
