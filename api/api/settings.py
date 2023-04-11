5e9ad1aa6b8d13d86a140c664dee7ab290967bc0
captjay98wsl <captjay98@gmail.com>
2023-04-11 05:01:39 +0100

    render

--- api/api/settings.py
+++ api/api/settings.py
@@ -92,13 +92,14 @@ WSGI_APPLICATION = "api.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = env("DATABASE_URL")
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
