16513f57d7ac615c949682e2b498e94d55abe826
captjay98wsl <captjay98@gmail.com>
2023-04-11 09:17:15 +0100

    again

--- api/api/settings.py
+++ api/api/settings.py
@@ -143,16 +143,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = "/static/"
-# Following settings only make sense on production and may break development environments.
-# in your application directory on Render.
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# Turn on WhiteNoise storage backend that takes care of compressing static files
-# and creating unique names for each version so they can safely be cached forever.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
