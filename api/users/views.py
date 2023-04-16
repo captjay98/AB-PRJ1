f79f79264f09ce8ad824174df14f0335ae20d952
captjay98wsl <captjay98@gmail.com>
2023-04-16 15:08:27 +0100

    Upated views in prep for google and github login

--- api/users/views.py
+++ api/users/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth import get_user_model
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import (
     RegisterView,
     SocialLoginView,
@@ -55,10 +58,14 @@ class CustomLoginView(LoginView):
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
+    # callback_url = CALLBACK_URL_YOU_SET_ON_GOOGLE
+    client_class = OAuth2Client
 
 
 class GithubLogin(SocialLoginView):
     adapter_class = GitHubOAuth2Adapter
+    # callback_url = # CALLBACK_URL_YOU_SET_ON_GITHUB
+    client_class = OAuth2Client
 
 
 class UserLogOutView(APIView):
