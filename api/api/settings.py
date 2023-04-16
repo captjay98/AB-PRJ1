7d4e0d461b518a298a4c9c30636c8788a81a9526
captjay98wsl <captjay98@gmail.com>
2023-04-16 05:29:45 +0100

    fix

--- api/api/settings.py
+++ api/api/settings.py
@@ -34,7 +34,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "https://interviewdemo.onrender.com",
+    "http://127.0.0.1",
+]
 
 # Application definition
 
