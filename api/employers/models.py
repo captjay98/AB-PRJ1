8a3afc01a383c024d2f98f7c5f5f546d1330b5c6
captjay98wsl <captjay98@gmail.com>
2023-05-05 11:43:52 +0100

    added the application model '

--- api/employers/models.py
+++ api/employers/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.db.models.fields import related
 from django.utils import timezone
 
+from seekers.models import SeekerProfile, Skill
+
 # Create your models here.
 
 
@@ -24,6 +26,7 @@ class Job(models.Model):
         on_delete=models.DO_NOTHING,
         related_name="jobRecruiter",
     )
+
     title = models.CharField(
         verbose_name="Job title",
         max_length=255,
@@ -38,12 +41,11 @@ class Job(models.Model):
         null=True,
     )
 
-    required_skills = models.TextField(
-        verbose_name="Required Skills",
-        max_length=255,
-        blank=True,
-        null=True,
-    )
+    # required_skills = models.ManyToManyField(
+    #     Skill,
+    #     verbose_name=("Required Skills"),
+    #     blank=True,
+    # )
 
     location = models.CharField(
         verbose_name="Location",
@@ -65,3 +67,54 @@ class Job(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Application(models.Model):
+    APPLICATION_STATUS = (
+        ("Submitted", "Submitted"),
+        ("Reviewing", "Reviewing"),
+        ("Accepted", "Accepted"),
+        ("Rejected", "Rejected"),
+    )
+
+    JOB_STATUS = (
+        ("open", "open"),
+        ("closed", "closed"),
+    )
+
+    applicant = models.ForeignKey(
+        SeekerProfile,
+        on_delete=models.DO_NOTHING,
+    )
+
+    employer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.DO_NOTHING,
+    )
+
+    job = models.ForeignKey(
+        Job,
+        on_delete=models.DO_NOTHING,
+    )
+
+    job_status = models.CharField(
+        verbose_name=("job_status"),
+        max_length=50,
+        choices=JOB_STATUS,
+        blank=True,
+        null=True,
+    )
+
+    application_status = models.CharField(
+        verbose_name=("Application_status"),
+        max_length=50,
+        choices=APPLICATION_STATUS,
+        blank=True,
+        null=True,
+    )
+
+    date_applied = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    # remars = moels.CharField("")
