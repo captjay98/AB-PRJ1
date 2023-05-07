ce177687b1d1952d90c0ce496ba159f4c461bb58
captjay98wsl <captjay98@gmail.com>
2023-05-07 12:54:08 +0100

    dob fix

--- api/seekers/models.py
+++ api/seekers/models.py
@@ -57,7 +57,7 @@ class SeekerProfile(models.Model):
         related_name="SeekerProfile",
     )
 
-    date_of_birth = models.DateField(
+    dob = models.DateField(
         verbose_name=_("Date of Birth"),
         blank=True,
         null=True,
