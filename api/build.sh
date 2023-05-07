a5c0bcc3aeb10ed9472df9355c8d3c36f0764bdb
captjay98wsl <captjay98@gmail.com>
2023-05-07 13:36:57 +0100

    updated build file

--- api/build.sh
+++ api/build.sh
@@ -5,10 +5,18 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate core
-python manage.py migrate users
-python manage.py migrate seekers
-python manage.py migrate employers
-python manage.py migrate
-python manage.py makemigrations
 
+python manage.py makemigrations core
+python manage.py migrate core
+
+python manage.py makemigrations users
+python manage.py migrate users
+
+python manage.py makemigrations seekers
+python manage.py migrate seekers
+
+python manage.py makemigrations employers
+python manage.py migrate employers
+
+python manage.py makemigrations
+python manage.py migrate
