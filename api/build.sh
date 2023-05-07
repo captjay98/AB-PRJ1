a11091cc6d44255d9e4f13cbc5feb17640e90851
captjay98wsl <captjay98@gmail.com>
2023-05-07 12:44:45 +0100

    build fie update

--- api/build.sh
+++ api/build.sh
@@ -5,10 +5,10 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate core
-python manage.py migrate users
-python manage.py migrate seekers
-python manage.py migrate employers
-python manage.py migrate
-python manage.py makemigrations
+# python manage.py migrate core
+# python manage.py migrate users
+# python manage.py migrate seekers
+# python manage.py migrate employers
+# python manage.py migrate
+# python manage.py makemigrations
 
