b4ef9cc351eadb465e63ad3426d65141e603ee8b
captjay98Zorin <captjay98@gmail.com>
2023-08-21 04:01:57 +0100

    trying a different build command 2.1.1.2

--- api/build.sh
+++ api/build.sh
@@ -1,12 +1,36 @@
 !/usr/bin/bash
-# exit on error
-# set -o errexit
 
+#apt update -y && apt upgrade -y && apt install -y --no-install-recommends binutils libstdc++6 libheif-dev libheif1 poppler-utils libjson-c-dev libproj-dev gdal-bin libgdal-dev python3-gdal
 
-apt update -y && apt upgrade -y && apt install -y --no-install-recommends binutils libproj-dev gdal-bin libgdal-dev python3-gdal python3.8-dev
+#ldconfig
+echo "FOUND THE JSON"
+find / -name  libjson-c.so.5
 
+#echo "THIS IS $LD_LIBRARY_PATH"
+echo "!!!!!!!!!!!!!!!!!!!"
 
+# cp /usr/lib/x86_64-linux-gnu/libpoppler.so.118 /usr/lib
+
+# cp  /usr/lib/x86_64-linux-gnu/libheif.so.1 /usr/lib
+
+# cp  /usr/lib/x86_64-linux-gnu/libjson-c.so.5 /usr/lib
+
+# cp  /usr/lib/x86_64-linux-gnu/libfreexl.so.1 /usr/lib
+
+# cp  /usr/lib/x86_64-linux-gnu/libqhull_r.so.8.0 /usr/lib
+
+# cp  -r /usr/lib/x86_64-linux-gnu/* /usr/lib
+
+echo "!!!!!!!!!!!!!!!!!!!!!!"
+echo "THIS IS $LD_LIBRARY_PATH"
+echo "FOUND THE FILEPATH"
 pip install --global-option=build_ext --global-option="-I/usr/include/gdal" GDAL==`gdal-config --version`
+
+# export LD_LIBRARY_PATH 
+# nix-env -iA nixpkgs.libheif
+# export LD_LIBRARY_PATH=/path/to/libheif/libs:$LD_LIBRARY_PATH
+# echo "What is going onnnn"
+# LD_LIBRARY_PATH=/nix/store/wprxx5zkkk13hpj6k1v6qadjylh3vq9m-gcc-11.3.0-lib/lib:/nix/store/zaflwh2nwzj1f0wngd7hqm3nvlf3yhsx-zlib-1.2.13/lib:/usr/lib
 
 
 # pip install -r requirements.txt
