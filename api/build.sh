f2c4e04f6195d625986df2d525f92d569862fd91
captjay98Zorin <captjay98@gmail.com>
2023-08-21 03:09:10 +0100

    removed gdal

--- api/build.sh
+++ api/build.sh
@@ -1,26 +1,29 @@
-# !/usr/bin/env bash
+!/usr/bin/bash
 # exit on error
 # set -o errexit
 
 
- apt update -y && apt upgrade -y && apt install -y --no-install-recommends
- binutils libproj-dev gdal-bin libgdal-dev python3-gdal
+apt update -y && apt upgrade -y && apt install -y --no-install-recommends binutils libproj-dev gdal-bin libgdal-dev python3-gdal python3.8-dev
 
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
 
-python manage.py makemigrations core
-python manage.py migrate core
+pip install --global-option=build_ext --global-option="-I/usr/include/gdal" GDAL==`gdal-config --version`
 
-python manage.py makemigrations users
-python manage.py migrate users
 
-python manage.py makemigrations seekers
-python manage.py migrate seekers
+# pip install -r requirements.txt
+# python manage.py collectstatic --no-input
 
-python manage.py makemigrations employers
-python manage.py migrate employers
+# python manage.py makemigrations core
+# python manage.py migrate core
 
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py makemigrations users
+# python manage.py migrate users
+
+# python manage.py makemigrations seekers
+# python manage.py migrate seekers
+
+# python manage.py makemigrations employers
+# python manage.py migrate employers
+
+# python manage.py makemigrations
+# python manage.py migrate
 
