3249e60e4ffb44344a662821ddb6ef1907052240
captjay98Zorin <captjay98@gmail.com>
2023-08-21 03:37:54 +0100

    trying a different build command

--- api/build.sh
+++ api/build.sh
@@ -1,26 +1,25 @@
-# !/usr/bin/env bash
+!/usr/bin/bash
 # exit on error
 # set -o errexit
 
 
- apt update -y && apt upgrade -y && apt install -y --no-install-recommends
- binutils libproj-dev gdal-bin libgdal-dev python3-gdal
+apt update -y && apt upgrade -y && apt install -y --no-install-recommends binutils libproj-dev gdal-bin libgdal-dev python3-gdal
 
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
+# pip install -r requirements.txt
+# python manage.py collectstatic --no-input
 
-python manage.py makemigrations core
-python manage.py migrate core
+# python manage.py makemigrations core
+# python manage.py migrate core
 
-python manage.py makemigrations users
-python manage.py migrate users
+# python manage.py makemigrations users
+# python manage.py migrate users
 
-python manage.py makemigrations seekers
-python manage.py migrate seekers
+# python manage.py makemigrations seekers
+# python manage.py migrate seekers
 
-python manage.py makemigrations employers
-python manage.py migrate employers
+# python manage.py makemigrations employers
+# python manage.py migrate employers
 
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py makemigrations
+# python manage.py migrate
 
